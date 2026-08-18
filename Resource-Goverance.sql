e3aa2dd570d0bc6adfe3e23e4768d3a835e6cbce
lunatic-def <111355545+lunatic-def@users.noreply.github.com>
2026-08-18 14:01:32 +0700

    Create Resource-Goverance.sql

---
+++ Resource-Goverance.sql
@@ -0,0 +1,9 @@
+-- CPU/Parrallelism Goverance 
+OPTION (MAXDOP 3) 
+
+-- Concurency/Lock Goverance
+WITH (ROWLOCK) 
+
+-- I/O and Optimizer Control force use a specific index
+WITH (INDEX (table_name))
+
